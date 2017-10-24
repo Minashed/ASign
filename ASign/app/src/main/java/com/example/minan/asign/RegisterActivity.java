3139826e04c21d7a8aee99f26659e715c7f6ad8f
Mina Nashed <mmnashed@myseneca.ca>
2017-10-24 05:07:24 -0400

    Functionality Added
    - Proper registration information sent to database

--- ASign/app/src/main/java/com/example/minan/asign/RegisterActivity.java
+++ ASign/app/src/main/java/com/example/minan/asign/RegisterActivity.java
@@ -1,9 +1,19 @@
 package com.example.minan.asign;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -21,5 +31,50 @@ public class RegisterActivity extends AppCompatActivity {
         final EditText etPNumber = (EditText) findViewById(R.id.etPNumber);
 
         final Button bRegisterR = (Button) findViewById(R.id.bRegisterR);
+
+        bRegisterR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String fname = etFName.getText().toString();
+                final String lname = etLName.getText().toString();
+
+                final String uname = etUNameR.getText().toString();
+                final String password = etPassword.getText().toString();
+
+                final String email = etEmail.getText().toString();
+                final String pnumber = etPNumber.getText().toString();
+
+                Response.Listener<String> responseListner = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success =  jsonResponse.getBoolean("success");
+
+                            if(success){
+                                Intent intent = new Intent (RegisterActivity.this, LoginActivity.class);
+                                RegisterActivity.this.startActivity(intent);
+                            }else{
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                builder.setMessage("Register Failed")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+
+                RegisterRequest registerRequest = new RegisterRequest(fname,lname,uname,password,email,pnumber,responseListner);
+
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+
+                queue.add(registerRequest);
+            }
+        });
     }
 }
---
+++ ASign/app/src/main/java/com/example/minan/asign/RegisterRequest.java
@@ -0,0 +1,37 @@
+package com.example.minan.asign;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Mina N on 10/23/2017.
+ */
+
+public class RegisterRequest extends StringRequest {
+
+    private static final String REGISTER_REQUEST_URL ="https://mmnashed.000webhostapp.com/ASign/Register.php";
+
+    private Map<String,String> params;
+
+    public RegisterRequest(String fname, String lname, String uname, String password, String email, String pnumber, Response.Listener<String> listener){
+        super(Method.POST, REGISTER_REQUEST_URL, listener, null); //Null is for and error listener
+        params = new HashMap<>();
+        params.put("fname", fname);
+        params.put("lname", lname);
+
+        params.put("uname", uname);
+        params.put("password", password);
+
+        params.put("email", email);
+        params.put("pnumber", pnumber);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+}
